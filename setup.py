898cf50b7f0a2ff47b0e71e40c434c3a886e6847
mdraws86 <m.draws@hotmail.de>
2024-05-01 19:03:38 +0200

    Write package

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name = "drop_needle",
+      version = "0.1",
+      description = "Simulate probability of a needle crossing a line on a table after it fell on it",
+      packages = ["drop_needle"],
+      zip_safe = False)
